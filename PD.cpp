6724b2d78b461df3bf7ddf460edb7ee7f715093f
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-10-18 17:33:22 +0100

    as does not respond

--- PD.cpp
+++ PD.cpp
@@ -13,12 +13,12 @@
 using namespace std;
 
 #define max(A,B)((A)>=(B)?(A):(B))
-#define PORT "58011"
+// #define PORT "58011"
 #define IP "tejo.tecnico.ulisboa.pt"
 #define SIZE 128
 
 extern int errno;
-char PDIP[SIZE], ASIP[SIZE], PDport[SIZE] = "57030", ASport[SIZE] = "58030";
+char PDIP[SIZE], ASIP[SIZE], PDport[SIZE] = "57030", ASport[SIZE] = "58011";
 char fixedReg[SIZE];
 
 void processInput(int argc, char* const argv[]) {
@@ -54,37 +54,18 @@ void processInput(int argc, char* const argv[]) {
 }
 
 int main(int argc, char* argv[]){
-<<<<<<< HEAD
     int udpServerSocket,udpClientSocket, afd = 0, errcode_c,errcode_s,fd;
     fd_set readfds;
     int maxfd, retval;
     ssize_t n;
     socklen_t addrlen_c, addrlen_s;
-    struct addrinfo hints_c,hints_s, *res_c, *res_s;
+    struct addrinfo hints_c, hints_s, *res_c, *res_s;
     struct sockaddr_in addr_c, addr_s;
     char buffer[SIZE], msg[SIZE];
     char command[3], uid[5], password[8];
 
     if (gethostname(ASIP ,SIZE) == -1)
         fprintf(stderr,"error: %s\n",strerror(errno));
-=======
-    int udpServerSocket, afd = 0, errcode;
-    fd_set readfds;
-    int maxfd, retval;
-    ssize_t n;
-    socklen_t addrlen;
-    struct addrinfo hints, *res;
-    struct sockaddr_in addr;
-    char buffer[SIZE], msg[SIZE];
-    char command[3], uid[5], password[8];
-
-    if (gethostname(ASIP ,SIZE) == -1)
-        fprintf(stderr,"error: %s\n",strerror(errno));
-
-
-    udpServerSocket = socket(AF_INET, SOCK_DGRAM, 0);//UDP socket
-    if(udpServerSocket == -1)/*error*/exit(1);
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
 
     udpClientSocket = socket(AF_INET, SOCK_DGRAM, 0);//UDP socket
         if(udpClientSocket == -1)/*error*/exit(1);
@@ -93,8 +74,8 @@ int main(int argc, char* argv[]){
     hints_c.ai_family = AF_INET;//IPv4
     hints_c.ai_socktype = SOCK_DGRAM;//UDP socket
 
-<<<<<<< HEAD
-     errcode_c = getaddrinfo(IP, PORT, &hints_c ,&res_c);
+    // errcode_c = getaddrinfo(IP, PORT, &hints_c ,&res_c);
+    errcode_c = getaddrinfo(IP, ASport, &hints_c ,&res_c);
         if (errcode_c != 0)/*error*/exit(1);
     
     udpServerSocket = socket(AF_INET, SOCK_DGRAM, 0);//UDP socket
@@ -105,35 +86,24 @@ int main(int argc, char* argv[]){
     hints_s.ai_socktype = SOCK_DGRAM;//UDP socket
     hints_s.ai_flags = AI_PASSIVE;
 
-     errcode_s = getaddrinfo(NULL, PORT, &hints_s ,&res_s);
+    // errcode_s = getaddrinfo(NULL, PORT, &hints_s ,&res_s);
+    errcode_s = getaddrinfo(NULL, ASport, &hints_s, &res_s);
         if (errcode_s != 0)/*error*/exit(1);
 
-    if(bind(udpServerSocket,res_s->ai_addr,res_s->ai_addrlen) < 0 )
-    {
-        printf (" Unable to bind socket \n");
-    }
-    else
-    {
-        printf (" Bound to socket .\n");
+    if (bind(udpServerSocket, res_s->ai_addr, res_s->ai_addrlen) < 0 ) {
+        printf ("Unable to bind socket\n");
+    } else {
+        printf ("Bound to socket\n");
     }
 
     processInput(argc, argv);
     
-    while(1){
+    while (1){
         FD_ZERO(&readfds);
         FD_SET(afd, &readfds);
         FD_SET(udpClientSocket, &readfds);
         FD_SET(udpServerSocket, &readfds);
         maxfd = max(udpClientSocket, udpServerSocket);
-=======
-    processInput(argc, argv);
-
-    while(1){
-        FD_ZERO(&readfds);
-        FD_SET(afd, &readfds);
-        FD_SET(udpServerSocket, &readfds);
-        maxfd = max(udpServerSocket, afd);
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
 
         retval = select(maxfd + 1, &readfds, NULL, NULL, NULL);
         if (retval <= 0)/*error*/exit(1);
@@ -144,7 +114,6 @@ int main(int argc, char* argv[]){
                 fgets(msg, SIZE, stdin);
                 strtok(msg, "\n");
                 if (strcmp(msg, "exit") == 0) {
-<<<<<<< HEAD
                     freeaddrinfo(res_c);
                     freeaddrinfo(res_s);
                     close(udpClientSocket);
@@ -154,61 +123,35 @@ int main(int argc, char* argv[]){
                 else {
                     strcat(strcat(msg, " "), fixedReg);              
                     n = sendto(udpClientSocket, msg, strlen(msg), 0, res_c->ai_addr, res_c->ai_addrlen);
+                    printf("%s\n", msg);
                     if (n == -1)/*error*/exit(1);       
                 }
                 memset(msg, '\0', SIZE * sizeof(char));
             }
-            else if (FD_ISSET(udpClientSocket, &readfds)) {
+            if (FD_ISSET(udpClientSocket, &readfds)) {
                 FD_CLR(udpClientSocket, &readfds);
-                addrlen_c=sizeof(addr_c);
-                n = recvfrom(udpClientSocket, buffer, 128, 0, (struct sockaddr*)&addr_c, &addrlen_c);
+                addrlen_c = sizeof(addr_c);
+                n = recvfrom(udpClientSocket, buffer, 128, 0, (struct sockaddr*) &addr_c, &addrlen_c);
                     if (n == -1)/*error*/exit(1);
 
                 write(1, buffer, n);
                 memset(buffer, '\0', SIZE * sizeof(char));
              }
-             else if (FD_ISSET(udpServerSocket, &readfds)) {
+            if (FD_ISSET(udpServerSocket, &readfds)) {
                 printf("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
                 FD_CLR(udpServerSocket, &readfds);
 
-                addrlen_s=sizeof(addr_s);
-                n = recvfrom(udpServerSocket, buffer, 128, 0, (struct sockaddr*)&addr_s, &addrlen_s);
+                addrlen_s = sizeof(addr_s);
+                n = recvfrom(udpServerSocket, buffer, 128, 0, (struct sockaddr*) &addr_s, &addrlen_s);
                     if (n == -1)/*error*/exit(1);
 
                 write(1, buffer, n);
 
-                n = sendto(udpServerSocket, msg, strlen(buffer), 0, res_s->ai_addr, res_s->ai_addrlen);
+                n = sendto(udpServerSocket, buffer, strlen(buffer), 0, res_s->ai_addr, res_s->ai_addrlen);
                     if (n == -1)/*error*/exit(1);   
 
-=======
-                    freeaddrinfo(res);
-                    close(udpServerSocket);
-                    exit(1);
-                }
-                else {
-                    strcat(strcat(msg, " "), fixedReg);    
-                    printf("to send : %s\n", msg);               
-                    n = sendto(udpServerSocket, msg, strlen(msg), 0, res->ai_addr, res->ai_addrlen);
-                    if (n == -1)/*error*/exit(1);                    
-                }
-                
-                memset(msg, '\0', SIZE * sizeof(char));
-            }
-            else if (FD_ISSET(udpServerSocket, &readfds)) {
-                FD_CLR(udpServerSocket, &readfds);
-                addrlen=sizeof(addr);
-                n = recvfrom(udpServerSocket, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
-                    if (n == -1)/*error*/exit(1);
-
-                write(1, buffer, n);
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
                 memset(buffer, '\0', SIZE * sizeof(char));
              }
         }
     }
-<<<<<<< HEAD
-=======
-    freeaddrinfo(res);
-    close(udpServerSocket);
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
 }
--- User.cpp
+++ User.cpp
@@ -1,229 +1,118 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
-#include <unistd.h>
 #include <string.h>
-#include <errno.h>
+#include <iostream>
 
 using namespace std;
 
-#define max(A,B)((A)>=(B)?(A):(B))
-#define PORT "58011"
+// #define PORT "58011"
 #define IP "tejo.tecnico.ulisboa.pt"
 #define SIZE 128
 
-<<<<<<< HEAD
-extern int errno;
-char PDIP[SIZE], ASIP[SIZE], PDport[SIZE] = "57030", ASport[SIZE] = "58030";
-char fixedReg[SIZE];
-=======
-char ASIP[SIZE], ASport[SIZE] = "57030", FSIP[SIZE], FSport[SIZE] = "59030";
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
+char ASIP[SIZE], ASport[SIZE] = "58011", FSIP[SIZE], FSport[SIZE] = "59030";
 
 void processInput(int argc, char* const argv[]) {
+
     //faltam verificacoes de qtds de argumentos etc acho eu
-    if (argc < 2)
-        exit(1);
-    strcpy(PDIP, argv[1]);
-    //PDIP = strdup(argv[1]);
-    for (int i = 2; i < argc - 1; i++) {
-        if (strcmp(argv[i], "-d") == 0) {
-            if (strlen(argv[i + 1]) > SIZE) exit(1);
-            strcpy(PDport, argv[i + 1]);
-            continue;
-        }
-        else if (strcmp(argv[i], "-n") == 0) {
-            if (strlen(argv[i + 1]) > SIZE) exit(1);
-            //ASIP = strdup(argv[i + 1]);
-            strcpy(ASIP, argv[i + 1]);
-            continue;
-        }
-        else if (strcmp(argv[i], "-p") == 0) {
-            if (strlen(argv[i + 1]) > SIZE) exit(1);
-            //ASport = strdup(argv[i + 1]);
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-p") == 0) {
             strcpy(ASport, argv[i + 1]);
             continue;
         }
-    }
-
-<<<<<<< HEAD
-    strcpy(fixedReg, PDIP);
-    strcat(fixedReg, " ");
-    strcat(fixedReg, PDport);
-    strcat(fixedReg, "\n");
-}
-=======
-int main(int argc, char* const argv[]) {
-    struct addrinfo hints, *res;
-    int fd, n;
-    ssize_t nbytes, nleft, nwritten, nread;
-    char *ptr, buffer[SIZE], msg[SIZE];
-    struct sigaction act;
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
-
-int main(int argc, char* argv[]){
-    int udpServerSocket,udpClientSocket, afd = 0, errcode_c,errcode_s,fd;
-    fd_set readfds;
-    int maxfd, retval;
-    ssize_t n;
-    socklen_t addrlen_c, addrlen_s;
-    struct addrinfo hints_c,hints_s, *res_c, *res_s;
-    struct sockaddr_in addr_c, addr_s;
-    char buffer[SIZE], msg[SIZE];
-    char command[3], uid[5], password[8];
-
-    if (gethostname(ASIP ,SIZE) == -1)
-        fprintf(stderr,"error: %s\n",strerror(errno));
-
-    udpClientSocket = socket(AF_INET, SOCK_DGRAM, 0);//UDP socket
-        if(udpClientSocket == -1)/*error*/exit(1);
-
-    memset(&hints_c, 0, sizeof hints_c);
-    hints_c.ai_family = AF_INET;//IPv4
-    hints_c.ai_socktype = SOCK_DGRAM;//UDP socket
-
-     errcode_c = getaddrinfo(IP, PORT, &hints_c ,&res_c);
-        if (errcode_c != 0)/*error*/exit(1);
-    
-    udpServerSocket = socket(AF_INET, SOCK_DGRAM, 0);//UDP socket
-    if(udpServerSocket == -1)/*error*/exit(1);
-
-    memset(&hints_s, 0, sizeof hints_s);
-    hints_s.ai_family = AF_INET;//IPv4
-    hints_s.ai_socktype = SOCK_DGRAM;//UDP socket
-    hints_s.ai_flags = AI_PASSIVE;
-
-     errcode_s = getaddrinfo(NULL, PORT, &hints_s ,&res_s);
-        if (errcode_s != 0)/*error*/exit(1);
-
-    if(bind(udpServerSocket,res_s->ai_addr,res_s->ai_addrlen) < 0 )
-    {
-        printf (" Unable to bind socket \n");
-    }
-    else
-    {
-        printf (" Bound to socket .\n");
-    }
-
-    processInput(argc, argv);
-<<<<<<< HEAD
-    
-    while(1){
-        FD_ZERO(&readfds);
-        FD_SET(afd, &readfds);
-        FD_SET(udpClientSocket, &readfds);
-        FD_SET(udpServerSocket, &readfds);
-        maxfd = max(udpClientSocket, udpServerSocket);
-
-        retval = select(maxfd + 1, &readfds, NULL, NULL, NULL);
-        if (retval <= 0)/*error*/exit(1);
-        
-        for (; retval; retval--){
-            if(FD_ISSET(afd, &readfds)){
-                FD_CLR(afd, &readfds);
-                fgets(msg, SIZE, stdin);
-                strtok(msg, "\n");
-                if (strcmp(msg, "exit") == 0) {
-                    freeaddrinfo(res_c);
-                    freeaddrinfo(res_s);
-                    close(udpClientSocket);
-                    close(udpServerSocket);
-                    exit(1);
-                }
-                else {
-                    strcat(strcat(msg, " "), fixedReg);              
-                    n = sendto(udpClientSocket, msg, strlen(msg), 0, res_c->ai_addr, res_c->ai_addrlen);
-                    if (n == -1)/*error*/exit(1);       
-                }
-                memset(msg, '\0', SIZE * sizeof(char));
-            }
-            else if (FD_ISSET(udpClientSocket, &readfds)) {
-                FD_CLR(udpClientSocket, &readfds);
-                addrlen_c=sizeof(addr_c);
-                n = recvfrom(udpClientSocket, buffer, 128, 0, (struct sockaddr*)&addr_c, &addrlen_c);
-                    if (n == -1)/*error*/exit(1);
-
-                write(1, buffer, n);
-                memset(buffer, '\0', SIZE * sizeof(char));
-             }
-             else if (FD_ISSET(udpServerSocket, &readfds)) {
-                printf("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
-                FD_CLR(udpServerSocket, &readfds);
-
-                addrlen_s=sizeof(addr_s);
-                n = recvfrom(udpServerSocket, buffer, 128, 0, (struct sockaddr*)&addr_s, &addrlen_s);
-                    if (n == -1)/*error*/exit(1);
-
-                write(1, buffer, n);
-
-                n = sendto(udpServerSocket, msg, strlen(buffer), 0, res_s->ai_addr, res_s->ai_addrlen);
-                    if (n == -1)/*error*/exit(1);   
-
-                memset(buffer, '\0', SIZE * sizeof(char));
-             }
+        else if (strcmp(argv[i], "-n") == 0) {
+            strcpy(ASIP, argv[i + 1]);
+            continue;
+        }
+        else if (strcmp(argv[i], "-m") == 0) {
+            strcpy(FSIP, argv[i + 1]);
+            continue;
+        }
+        else if (strcmp(argv[i], "-q") == 0) {
+            strcpy(FSport, argv[i + 1]);
+            continue;
         }
     }
-=======
+}
 
-    fgets(msg, SIZE, stdin);
+int main(int argc, char* const argv[]) {
+    struct addrinfo hints, *res;
+    int tcpServerSocket, errcode, rID;
+    ssize_t n;
+    ssize_t nbytes, nleft, nwritten, nread;
+    char *ptr, buffer[SIZE], msg[SIZE], command[4], fop[2], uid[6];
 
-    ptr = (char*) malloc(strlen(msg) + 1);
-    strcpy(ptr, msg);
-
-    strcat(ptr, "\0");
-    
-    nbytes = strlen(ptr);
-
-    fd = socket(AF_INET, SOCK_STREAM, 0);//TCP socket
-    if (fd == -1)/*error*/exit(1);
-
+    gethostname(FSIP, SIZE);
+    gethostname(ASIP, SIZE);
+  
+    tcpServerSocket = socket(AF_INET, SOCK_STREAM, 0);//TCP socket
+    if (tcpServerSocket == -1)/*error*/exit(1);
+   
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;//IPv4
     hints.ai_socktype = SOCK_STREAM;//TCP socket
 
-    memset(&hints, 0, sizeof(act));
-    act.sa_handler = SIG_IGN;
+    // errcode = getaddrinfo(IP, PORT, &hints, &res);  
+    errcode = getaddrinfo(IP, ASport, &hints, &res);  
+    if (errcode != 0)/*error*/exit(1);
 
-    if (sigaction(SIGPIPE, &act, NULL) == -1)/*error*/exit(1);
+    n = connect(tcpServerSocket, res->ai_addr, res->ai_addrlen);
+        if (n == -1)/*error*/{exit(1);} 
 
-    n = getaddrinfo(IP, PORT, &hints, &res);  
-    if (n != 0)/*error*/exit(1);
+    processInput(argc, argv);
 
-    n = connect(fd, res->ai_addr, res->ai_addrlen);
-    if (n == -1)/*error*/{
-        //printf("%u", res->ai_addrlen);
-        //printf("connect\n");
-        exit(1);
+    while (1) {
+        fgets(msg, SIZE, stdin);
+        ptr = (char*) malloc(strlen(msg) + 1);
+        strcpy(ptr, msg);
+        strcat(ptr, "\0");
+        nbytes = strlen(ptr);
+
+        if (strcmp(msg, "exit\n") == 0) {
+            close(tcpServerSocket);
+            exit(1);
+        } 
+
+        else {
+            char *token = strtok(msg, " ");
+            strcpy(command, token);
+
+            if (strcmp(command, "LOG") == 0) {
+                token = strtok(NULL, " ");
+                strcpy(uid, token);
+            }
+            else if (strcmp(command, "REQ") == 0) {
+                if (uid == NULL) {
+                    printf("NO UID\n");
+                    exit(1);
+                }
+                rID = rand() % 9000 + 1000;
+
+                while( token != NULL ) {
+                    printf( "%s\n", token);
+                    token = strtok(NULL, " ");
+                }
+
+                // strcat(command, uid, 5);
+                // printf("%s\n", command);
+            }
+            else if (strcmp(command, "AUT") == 0) {
+                ;;
+            }
+        }
+        
+        /*else{
+            nleft = nbytes;
+                n = write(tcpServerSocket, ptr, nleft);
+                if (n <= 0) exit(1);
+            
+                n = read(tcpServerSocket, buffer, SIZE);
+                if (n == -1)  exit(1);
+               
+                write(1, buffer, n);   
+            }*/
     }
-
-    nleft = nbytes;
-    while (nleft > 0) {
-        nwritten = write(fd, ptr, nleft);
-        printf("write\n");
-        if (nwritten <= 0)/*error*/exit(1);
-        nleft -= nwritten;
-        ptr += nwritten;
-}
-    nleft = nbytes; 
-    ptr = buffer;
-    while (nleft > 0) {
-        nread = read(fd, ptr, nleft);
-        printf("read: %ld\n", nread);
-        if (nread == -1)/*error*/ exit(1);
-        else if (nread == 0) break;//closed by peer
-        nleft -= nread;
-        ptr += nread;
-    }
-    nread = nbytes - nleft;  
-    close(fd);
-
-    write(1, buffer, nread);
-    printf("escreveu");
-    exit(0);
->>>>>>> 716cfb8107dcd4782dd8026def24fbf435e63b45
 }
